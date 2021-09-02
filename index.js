64f3a89f2da8681a7021f2b4dec7ef76515692e0
Sebanaza <sebanazarian@gmail.com>
2021-09-01 22:26:17 -0300

    Implement Bom

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const app = express();
 const {config} = require('./config/index');
 const moviesApi = require('./routes/movies.js');
 
-const { logErrors, errorHandler} = require('./utils/middleware/errorHandlers.js')
+const { logErrors, errorHandler, wrapErrors} = require('./utils/middleware/errorHandlers.js')
 
+const notFoundHandler = require('./utils/middleware/notFoundHandler.js');
 
 app.use(express.json()) //permite a nuestras rutas datos de formatos json interpretarlos Middleware Body parser
 
-
+// routes
 moviesApi(app);
 
+//captura el error 404
+app.use(notFoundHandler);
+
+//MANEJADORES DE ERRORES
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function() {
--- utils/middleware/errorHandlers.js
+++ utils/middleware/errorHandlers.js
@@ -1,8 +1,12 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
+
 
 function withErrorStack(error, stack) {
   if (config.dev) {
-    return { error, stack };
+    // return { error, stack }; sin boom
+    return { ...error, stack };
+
   }
 
   return error;
@@ -13,12 +17,28 @@ function logErrors(err, req, res, next) {
   next(err);
 }
 
+function wrapErrors(err,req,res,next){
+  if(!err.isBoom){ //si el error no es Boom
+    next(boom.badImplementation(err)); //manera generica de marcar el error
+  }
+
+  next(err); //llamamos al siguiente middleware
+}
+
 function errorHandler(err, req, res, next) { // eslint-disable-line
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const {output:{statusCode,payload}}= err;  //viene con esa estructura del boom
+
+
+  // res.status(err.status || 500);
+  res.status(statusCode);
+
+  // res.json(withErrorStack(err.message, err.stack));
+  res.json(withErrorStack(payload, err.stack));
+
 }
 
 module.exports = {
   logErrors,
+  wrapErrors,
   errorHandler
 };
